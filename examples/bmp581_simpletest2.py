478ba3e8b94f713af96aa57bb97744ab48e298ba
mosimosidesu <mosimosidesu@gmail.com>
2024-03-25 18:03:27 +0900

    aaa

---
+++ examples/bmp581_simpletest2.py
@@ -0,0 +1,16 @@
+# SPDX-FileCopyrightText: Copyright (c) 2023 Jose D. Montoya
+#
+# SPDX-License-Identifier: MIT
+
+import bmp581
+
+bmp = bmp581.BMP581()
+
+# Oddly first pressure measure after power-on return wrong value.
+# so we "burn" one measure
+_ = bmp.pressure
+
+
+print(f"気温: {bmp.temperature:.2f}°C")
+print(f"気圧: {bmp.pressure:.2f}hPa")
+print(f"高度: {bmp.altitude:.1f}m")
