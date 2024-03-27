813938cddc1709fd72a3799ecaee1da5510774c3
mosimosidesu <mosimosidesu@gmail.com>
2024-03-27 10:45:53 +0900

    aaa

--- bmp581_simpletest2.py
+++ bmp581_simpletest2.py
@@ -9,8 +9,7 @@ bmp = bmp581.BMP581()
 # Oddly first pressure measure after power-on return wrong value.
 # so we "burn" one measure
 _ = bmp.pressure
-_ = bmp.altitude
 
 print(f"気温: {bmp.temperature:.2f}°C")
-print(f"気圧: {bmp.pressure:.2f}hPa")
+print(f"気圧: {bmp.pressure * 10:.2f}hPa")
 print(f"高度: {bmp.altitude:.1f}m")
